cca0c5e96b63a3058e3708474712a1611d168fea
Adam VanIwaarden <avaniwaarden@gmail.com>
2020-06-09 11:21:59 -0600

    Old uncommitted changes

--- WIDA_CO_Data_LONG_2019.R
+++ WIDA_CO_Data_LONG_2019.R
@@ -11,12 +11,7 @@ require(SGP)
 
 
 ### Load data
-tmp.dir <- getwd()
-setwd(tempdir())
-system(paste0("unzip '", file.path(tmp.dir, "Data/Base_Files/ACCESS19_SGP_LongFile_Readin.zip"), "'"))
-WIDA_CO_Data_LONG_2019 <- fread("ACCESS19_SGP_LongFile_Readin.csv", colClasses=rep("character", 23))
-unlink("ACCESS19_SGP_LongFile_Readin.csv")
-setwd(tmp.dir)
+WIDA_CO_Data_LONG_2019 <- fread("Data/Base_Files/NCIEA (Marie Huchton)/WIDA_Readin_2019_EDWD_06.04.19.csv", colClasses=rep("character", 24))
 
 
 ### Tidy up data
@@ -30,7 +25,8 @@ WIDA_CO_Data_LONG_2019[, SCALE_SCORE := as.numeric(SCALE_SCORE)]
 
 # WIDA_CO_Data_LONG_2019[ACHIEVEMENT_LEVEL == "", ACHIEVEMENT_LEVEL := NA] # table(WIDA_CO_Data_LONG_2019[, is.na(SCALE_SCORE), ACHIEVEMENT_LEVEL])
 WIDA_CO_Data_LONG_2019[, ACHIEVEMENT_LEVEL := as.factor(ACHIEVEMENT_LEVEL)]
-levels(WIDA_CO_Data_LONG_2019$ACHIEVEMENT_LEVEL) <- c("NO SCORE", "L1", "L2", "L3", "L4", "L5", "L6")
+levels(WIDA_CO_Data_LONG_2019$ACHIEVEMENT_LEVEL) <- c("L1", "L2", "L3", "L4", "L5", "L6", "NO SCORE")
+WIDA_CO_Data_LONG_2019[, ACHIEVEMENT_LEVEL := as.character(ACHIEVEMENT_LEVEL)]
 
 WIDA_CO_Data_LONG_2019[GENDER=="MISSING", GENDER := NA]
 WIDA_CO_Data_LONG_2019[, GENDER := as.factor(GENDER)]
@@ -43,7 +39,7 @@ levels(WIDA_CO_Data_LONG_2019$ETHNICITY) <- sapply(levels(WIDA_CO_Data_LONG_2019
 WIDA_CO_Data_LONG_2019[FREE_REDUCED_LUNCH_STATUS == "MISSING", FREE_REDUCED_LUNCH_STATUS := NA]
 WIDA_CO_Data_LONG_2019[, FREE_REDUCED_LUNCH_STATUS := as.factor(FREE_REDUCED_LUNCH_STATUS)]
 
-WIDA_CO_Data_LONG_2019[ELL_STATUS == "MISSING", ELL_STATUS := NA]
+WIDA_CO_Data_LONG_2019[ELL_STATUS == "Missing", ELL_STATUS := NA] # NOTE change in 2019 to camel case
 WIDA_CO_Data_LONG_2019[, ELL_STATUS := as.factor(ELL_STATUS)]
 
 WIDA_CO_Data_LONG_2019[IEP_STATUS == "MISSING", IEP_STATUS := NA]
@@ -60,8 +56,9 @@ WIDA_CO_Data_LONG_2019[, SCHOOL_ENROLLMENT_STATUS := as.factor(SCHOOL_ENROLLMENT
 WIDA_CO_Data_LONG_2019[, DISTRICT_ENROLLMENT_STATUS := as.factor(DISTRICT_ENROLLMENT_STATUS)]
 
 
-### Setkey and invalidate duplicate cases
+### Setkey and invalidate duplicate cases (0 new invalidated in 2019 - 2000 already done by Marie)
 
+table(WIDA_CO_Data_LONG_2019[, VALID_CASE])
 setkey(WIDA_CO_Data_LONG_2019, VALID_CASE, CONTENT_AREA, YEAR, GRADE, ID, SCALE_SCORE)
 setkey(WIDA_CO_Data_LONG_2019, VALID_CASE, CONTENT_AREA, YEAR, GRADE, ID)
 WIDA_CO_Data_LONG_2019[which(duplicated(WIDA_CO_Data_LONG_2019, by=key(WIDA_CO_Data_LONG_2019)))-1, VALID_CASE:="INVALID_CASE"]
--- WIDA_CO_SGP_2019.R
+++ WIDA_CO_SGP_2019.R
@@ -30,16 +30,27 @@ WIDA_CO_SGP <- abcSGP(
 		sgp.percentiles.equated=FALSE,
 		sgp.target.scale.scores=TRUE,
 		save.intermediate.results=FALSE,
-		sgPlot.demo.report=TRUE,
 		parallel.config=list(
 			BACKEND="PARALLEL",
 			WORKERS=list(
-				PERCENTILES=6,
-				PROJECTIONS=6,
-				LAGGED_PROJECTIONS=6,
-				SGP_SCALE_SCORE_TARGETS=6,
-				SUMMARY=6)))
+				PERCENTILES=13,
+				PROJECTIONS=13,
+				LAGGED_PROJECTIONS=13,
+				SGP_SCALE_SCORE_TARGETS=13,
+				SUMMARY=20)))
 
+
+###   Re-run combineSGP with multiple year TARGET_SCALE_SCORES (Added 5/14/2020)
+
+WIDA_CO_SGP@SGP[["SGProjections"]][["READING.2019.TARGET_SCALE_SCORES"]] <- NULL
+WIDA_CO_SGP@SGP[["SGProjections"]][["READING.2019.LAGGED.TARGET_SCALE_SCORES"]] <- NULL
+
+WIDA_CO_SGP <- combineSGP(
+		WIDA_CO_SGP,
+		years = 2019,
+		sgp.target.scale.scores=TRUE,
+		max.sgp.target.years.forward=1:6
+)
 
 ### Save results
 
@@ -54,5 +65,5 @@ visualizeSGP(
 		sgPlot.years='2019',
 		sgPlot.content_areas="READING",
 		# sgPlot.year.span=3, # Now in SGPstateData
-		sgPlot.demo.report=TRUE,
-		parallel.config=list(BACKEND="PARALLEL", WORKERS=list(SG_PLOTS=6)))
+		# sgPlot.demo.report=TRUE,
+		parallel.config=list(BACKEND="PARALLEL", WORKERS=list(SG_PLOTS=20)))
